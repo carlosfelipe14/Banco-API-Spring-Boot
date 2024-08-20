f7ae5924dd73822973e53458ddcee5f4476e637d
Carlos Ramos <carlos.dg147@gmail.com>
2024-08-20 17:38:30 -0400

    Conectando a la BD mariadb y creando la entidad 'clientes'

---
+++ src/main/java/com/practica/banco/models/Clientes.java
@@ -0,0 +1,116 @@
+package com.practica.banco.models;
+
+import com.practica.banco.dtos.Genero;
+import com.practica.banco.dtos.TipoDoc;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import java.util.Date;
+
+@Entity
+public class Clientes {
+    
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(length = 100)
+    private String nombre;
+    @Column(length = 50)
+    private String primerApellido;
+    @Column(length = 50)
+    private String segundoApellido;
+    @Enumerated(EnumType.STRING)
+    private TipoDoc tipoDocumento;
+    @Column(length = 30)
+    private String numeroDocumento;
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+    @Enumerated(EnumType.ORDINAL)
+    private Genero genero;
+
+    public Clientes() {}
+
+    public Clientes(int id, String nombre, String primerApellido, String segundoApellido, TipoDoc tipoDocumento,
+            String numeroDocumento, Date fechaNacimiento, Genero genero) {
+        this.id = id;
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public TipoDoc getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDoc tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+}
