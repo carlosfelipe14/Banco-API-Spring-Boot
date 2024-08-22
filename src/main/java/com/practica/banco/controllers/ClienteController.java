dd59a984b20035b8ba6caaf9db1c71e244b40a08
Carlos Ramos <carlos.dg147@gmail.com>
2024-08-22 19:05:46 -0400

    Añadiendo code status 201 en POST/clientes. Se usó la forma más básica.

--- src/main/java/com/practica/banco/controllers/ClienteController.java
+++ src/main/java/com/practica/banco/controllers/ClienteController.java
@@ -3,6 +3,8 @@ package com.practica.banco.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,8 +30,8 @@ public class ClienteController {
     }
 
     @PostMapping()
-    public ClienteDTO create(@RequestBody ClienteDTO cliente) {
-        return clienteService.save(cliente);
+    public ResponseEntity<ClienteDTO> create(@RequestBody ClienteDTO cliente) {
+        return new ResponseEntity<>(clienteService.save(cliente), HttpStatus.CREATED);
     }
 
     @GetMapping("/{cluuid}")
